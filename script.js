e01603be7864aadd0e594de648115dc3c0589926
Akash Dixit <137980846+Akash-dixit24@users.noreply.github.com>
2025-11-29 12:52:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const btn = document.querySelector('input[type="button"]');
+const select = document.getElementById("colorSelect");
+
+btn.addEventListener("click", () => {
+  // Find selected option
+  const selectedIndex = select.selectedIndex;
+
+  // If a valid option is selected, remove it
+  if (selectedIndex !== -1) {
+    select.remove(selectedIndex);
+  }
+});
+
